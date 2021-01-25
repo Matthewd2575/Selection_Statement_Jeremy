90380f26d09fa659f83aa93eb407273f38d45d27
Matthew Daugherty <Matthewd2575@gmail.com>
2021-01-25 14:33:01 -0700

    Jeremy's version of example

---
+++ Selection Statement (Jeremy)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Selection_Statement__Jeremy_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine($"Guess a number between 1-10");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+
+            else if (userInput > favNumber)
+
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+            else
+
+            { 
+                Console.WriteLine($"NEVERMIND!"); 
+            }
+        
+            
+
+
+            
+        }
+    }
+}
